ace798eb8f68dc348a44c408eea72a672011b271
Julian Joonyon Park <joonyon0807@gmail.com>
2016-04-26 18:03:43 -0700

    sign

--- script.js
+++ script.js
@@ -81,6 +81,8 @@ var player = {
                         console.log(d3.select(this));
                         d3.select('.start_container')
                             .remove();
+                        d3.select('.start_sign')
+                            .remove();
                         currplayer = eval(d3.select(this).property('id'));
                         currplayerval = d3.select(this).property('id');
                         cancompare = 1;
@@ -251,7 +253,7 @@ var player = {
              d3.selectAll("svg").remove();
              d3.selectAll('.poster_section')
                 .append('div')
-                // .attr('class', 'start_container')
+                .attr('class', 'start_container')
                 .attr('position', 'relative')
                 .attr('top', '-1000')
                 .append('img')
@@ -637,7 +639,7 @@ $(document).ready(function () {
           d3.selectAll(".start_sign").remove();
           d3.selectAll('.poster_section')
              .append('div')
-            //  .attr('class', 'start_container')
+             .attr('class', 'start_container')
              .attr('position', 'relative')
              .attr('top', '-1000')
              .append('img')
